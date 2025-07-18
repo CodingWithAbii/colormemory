6c3490bc6e2dfa20a7e1c34627024f4639f8b970
codewithabii <julardzijaa.abdullah@gmail.com>
2025-07-18 11:59:43 +0200

    #3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4 font-body">
       <ColorMemoryChallenge />
+       <a href="https://github.com/CodingWithAbii/colormemory" target="_blank" rel="noopener noreferrer">
+        View code on GitHub
+      </a>
     </main>
   );
 }
